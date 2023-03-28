027fc100dd71b0ef1886317525a02d54c16be4cf
carelle <raffaele.carelle@algoritma.it>
2023-03-28 16:59:44 +0200

    test post install shel exec

--- src/Installer/Installer.php
+++ src/Installer/Installer.php
@@ -193,7 +193,7 @@ class Installer
 
         $this->phpCsWriter->writeConfigFile($this->projectRoot . '/.php-cs-fixer.dist.php', false, true);
 
-        shell_exec("cp ../../phpstan.php {$this->projectRoot}/phpstan.php");
+        shell_exec("pwd");
     }
 
     public function requestAddComposerScripts(): void
