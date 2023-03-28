18c7f0d480a024c417b8ed27a495dfb921481e93
carelle <raffaele.carelle@algoritma.it>
2023-03-28 17:22:52 +0200

    relative path to rules

--- phpstan.php
+++ phpstan.php
@@ -10,8 +10,8 @@ $includes = [];
 $symfony = getenv('SYMFONY');
 if ($symfony) {
     $includes = array_merge($includes, [
-        '/tools/vendor/phpstan/phpstan-symfony/extension.neon',
-        '/tools/vendor/phpstan/phpstan-symfony/rules.neon'
+        'vendor/phpstan/phpstan-symfony/extension.neon',
+        'vendor/phpstan/phpstan-symfony/rules.neon'
     ]);
 
     $containerXmlPath = getenv('SYMFONY_CONTAINER_XML_PATH', true);
@@ -39,8 +39,8 @@ $doctrine = getenv('DOCTRINE');
 
 if ($doctrine) {
     $includes = array_merge($includes, [
-        '/tools/vendor/phpstan/phpstan-doctrine/extension.neon',
-        '/tools/vendor/phpstan/phpstan-doctrine/rules.neon'
+        'vendor/phpstan/phpstan-doctrine/extension.neon',
+        'vendor/phpstan/phpstan-doctrine/rules.neon'
     ]);
 }
 
@@ -48,7 +48,7 @@ $magento = getenv('MAGENTO');
 
 if ($magento) {
     $includes = array_merge($includes, [
-        '/tools/vendor/bitexpert/phpstan-magento/extension.neon'
+        'vendor/bitexpert/phpstan-magento/extension.neon'
     ]);
 }
 
@@ -56,7 +56,7 @@ $laravel = getenv('LARAVEL');
 
 if ($laravel) {
     $includes = array_merge($includes, [
-        '/tools/vendor/nunomaduro/larastan/extension.neon'
+        'vendor/nunomaduro/larastan/extension.neon'
     ]);
 }
 
