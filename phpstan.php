4c1daf8dd8194412eb190755ef9e5073306825cb
carelle <raffaele.carelle@algoritma.it>
2023-03-28 15:58:36 +0200

    add config files

---
+++ phpstan.php
@@ -0,0 +1,56 @@
+<?php
+
+$parameters = [
+    'parameters' => [
+        'level'        => 6,
+        'fileExtensions' => ['php']
+    ],
+];
+
+$includes = [];
+
+$symfony = getenv('SYMFONY');
+if($symfony) {
+    $includes = array_merge($includes, [
+        '/tools/vendor/phpstan/phpstan-symfony/extension.neon',
+        '/tools/vendor/phpstan/phpstan-symfony/rules.neon'
+    ]);
+
+    $containerXmlPath = getenv('SYMFONY_CONTAINER_XML_PATH');
+
+    $parameters = array_merge($parameters, [
+        'symfony' => [
+            'containerXmlPath' => 'var/cache/dev/AppKernelDevDebugContainer.xml'
+        ]
+    ]);
+}
+
+$doctrine = getenv('DOCTRINE');
+
+if($doctrine) {
+    $includes = array_merge($includes, [
+        '/tools/vendor/phpstan/phpstan-doctrine/extension.neon',
+        '/tools/vendor/phpstan/phpstan-doctrine/rules.neon'
+    ]);
+}
+
+$magento = getenv('MAGENTO');
+
+if($magento) {
+    $includes = array_merge($includes, [
+        '/tools/vendor/bitexpert/phpstan-magento/extension.neon'
+    ]);
+}
+
+$laravel = getenv('LARAVEL');
+
+if($laravel) {
+    $includes = array_merge($includes, [
+        '/tools/vendor/nunomaduro/larastan/extension.neon'
+    ]);
+}
+
+return [
+    'includes' => $includes,
+    'parameters' => $parameters
+];
