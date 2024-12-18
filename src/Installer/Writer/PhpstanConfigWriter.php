9c3635c9feaa0744cdbc40697cbc23e7d3be1387
Raffaele Carelle <raffaele.carelle@algoritma.it>
2024-12-18 10:57:21 +0100

    Remove unused import from PhpstanConfigWriter
    The `Composer\InstalledVersions` import was unnecessary and removed to clean up the code. This improves readability and maintains a lean codebase.

--- src/Installer/Writer/PhpstanConfigWriter.php
+++ src/Installer/Writer/PhpstanConfigWriter.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Algoritma\CodingStandards\Installer\Writer;
 
-use Composer\InstalledVersions;
 use Nette\Neon\Neon;
 
 final class PhpstanConfigWriter implements PhpCsConfigWriterInterface
