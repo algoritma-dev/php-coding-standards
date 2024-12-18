a6e609182457a9e5944997f738c0c94281f12b57
Raffaele Carelle <raffaele.carelle@algoritma.it>
2024-12-18 10:52:34 +0100

    Refactor Phpstan config writer to improve modularity
    Split PhpstanConfigWriter into multiple methods for cleaner code and modular design. Added support for separate configuration files for rules and paths. Updated tests to validate the new configuration structure and behavior.

--- tests/Installer/Writer/PhpstanConfigWriterTest.php
+++ tests/Installer/Writer/PhpstanConfigWriterTest.php
@@ -23,89 +23,137 @@ class PhpstanConfigWriterTest extends TestCase
     public function testWriteConfigFile(): void
     {
         $filename = $this->vfsRoot->url() . '/phpstan.neon';
+        $rulesFilename = $this->vfsRoot->url() . '/phpstan-rules.php';
+        $pathsFilename = $this->vfsRoot->url() . '/phpstan-paths.php';
+
         $writer = new PhpstanConfigWriter();
 
         $writer->writeConfigFile($filename);
 
         $content = file_get_contents($filename);
+        $rulesContent = file_get_contents($rulesFilename);
+        $pathsContent = file_get_contents($pathsFilename);
 
         $expected
             = <<<'EOD'
+                includes:
+                	- phpstan-rules.php
+                	- phpstan-paths.php
+
                 parameters:
                 	level: 8
-                	paths:
-                		- src/
-                		- tests/
+                	excludePaths:
+                		- **/node_modules/*
+                		- **/vendor/*
+
+                	type_perfect:
+                		no_mixed_property: true
+                		no_mixed_caller: true
+                		null_over_false: true
+                		narrow_param: true
+                		narrow_return: true
+
+                	errorFormat: ticketswap
+                	editorUrl: 'phpstorm://open?file=%%file%%&line=%%line%%'
+                	ignoreErrors: []
 
 
                 EOD;
-
         $this->assertSame($expected, $content);
+
+        $expected
+            = <<<'EOD'
+                <?php
+
+                $additionalRules = [];
+                $rulesProvider = new Algoritma\CodingStandards\Rules\CompositeRulesProvider([
+                    new Algoritma\CodingStandards\Rules\PhpstanRulesProvider(),
+                    new Algoritma\CodingStandards\Rules\ArrayRulesProvider($additionalRules),
+                ]);
+
+                return ['rules' => $rulesProvider->getRules()];
+
+                EOD;
+
+        $this->assertSame($expected, $rulesContent);
+
+        $expected
+            = <<<'EOD'
+                <?php
+
+                $autoloadPathProvider = new Algoritma\CodingStandards\AutoloadPathProvider();
+                return ['parameters' => ['paths' => [$autoloadPathProvider->getPaths()]]];
+
+                EOD;
+
+        $this->assertSame($expected, $pathsContent);
     }
 
     public function testWriteConfigFileWithNoDev(): void
     {
         $filename = $this->vfsRoot->url() . '/phpstan.neon';
+        $rulesFilename = $this->vfsRoot->url() . '/phpstan-rules.php';
+        $pathsFilename = $this->vfsRoot->url() . '/phpstan-paths.php';
         $writer = new PhpstanConfigWriter();
 
         $writer->writeConfigFile($filename, true);
 
         $content = file_get_contents($filename);
+        $rulesContent = file_get_contents($rulesFilename);
+        $pathsContent = file_get_contents($pathsFilename);
 
         $expected
             = <<<'EOD'
+                includes:
+                	- phpstan-rules.php
+                	- phpstan-paths.php
+
                 parameters:
                 	level: 8
-                	paths:
-                		- src/
+                	excludePaths:
+                		- **/node_modules/*
+                		- **/vendor/*
+
+                	type_perfect:
+                		no_mixed_property: true
+                		no_mixed_caller: true
+                		null_over_false: true
+                		narrow_param: true
+                		narrow_return: true
+
+                	errorFormat: ticketswap
+                	editorUrl: 'phpstorm://open?file=%%file%%&line=%%line%%'
+                	ignoreErrors: []
 
 
                 EOD;
-
         $this->assertSame($expected, $content);
-    }
-
-    public function testWriteConfigFileWithNoRisky(): void
-    {
-        $filename = $this->vfsRoot->url() . '/phpstan.neon';
-        $writer = new PhpstanConfigWriter();
-
-        $writer->writeConfigFile($filename, false, true);
-
-        $content = file_get_contents($filename);
 
         $expected
             = <<<'EOD'
-                parameters:
-                	level: 8
-                	paths:
-                		- src/
-                		- tests/
+                <?php
 
+                $additionalRules = [];
+                $rulesProvider = new Algoritma\CodingStandards\Rules\CompositeRulesProvider([
+                    new Algoritma\CodingStandards\Rules\PhpstanRulesProvider(),
+                    new Algoritma\CodingStandards\Rules\ArrayRulesProvider($additionalRules),
+                ]);
+
+                return ['rules' => $rulesProvider->getRules()];
 
                 EOD;
 
-        $this->assertSame($expected, $content);
-    }
-
-    public function testWriteConfigFileWithNoDevAndNoRisky(): void
-    {
-        $filename = $this->vfsRoot->url() . '/phpstan.neon';
-        $writer = new PhpstanConfigWriter();
-
-        $writer->writeConfigFile($filename, true, true);
-
-        $content = file_get_contents($filename);
+        $this->assertSame($expected, $rulesContent);
 
         $expected
-= <<<'EOD'
-    parameters:
-    	level: 8
-    	paths:
-    		- src/
+            = <<<'EOD'
+                <?php
 
+                $autoloadPathProvider = new Algoritma\CodingStandards\AutoloadPathProvider(null, null, false);
+                return ['parameters' => ['paths' => [$autoloadPathProvider->getPaths()]]];
 
-    EOD;
-        $this->assertSame($expected, $content);
+                EOD;
+
+        $this->assertSame($expected, $pathsContent);
     }
 }
