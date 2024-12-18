3d2048272e5ff8d1231e69522894a0277aba7ed6
Raffaele Carelle <raffaele.carelle@algoritma.it>
2024-12-18 10:52:34 +0100

    Refactor Phpstan config writer to improve modularity
    Split PhpstanConfigWriter into multiple methods for cleaner code and modular design. Added support for separate configuration files for rules and paths. Updated tests to validate the new configuration structure and behavior.

--- src/Installer/Writer/PhpstanConfigWriter.php
+++ src/Installer/Writer/PhpstanConfigWriter.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Algoritma\CodingStandards\Installer\Writer;
 
-use Algoritma\CodingStandards\AutoloadPathProvider;
+use Composer\InstalledVersions;
 use Nette\Neon\Neon;
 
 final class PhpstanConfigWriter implements PhpCsConfigWriterInterface
@@ -12,22 +12,86 @@ final class PhpstanConfigWriter implements PhpCsConfigWriterInterface
     public function writeConfigFile(?string $filename = null, bool $noDev = false, bool $noRisky = false): void
     {
         $filename = $filename ?: 'phpstan.neon';
-        file_put_contents($filename, $this->createConfigSource($noDev));
+        file_put_contents($filename, $this->createConfigSource());
+        file_put_contents(dirname($filename) . '/phpstan-rules.php', $this->createRulesFile());
+        file_put_contents(dirname($filename) . '/phpstan-paths.php', $this->createPathsFile($noDev));
     }
 
-    private function createConfigSource(bool $noDev = false): string
+    private function createConfigSource(): string
     {
-        $autoloadPathProvider = new AutoloadPathProvider();
-
-        if ($noDev) {
-            $autoloadPathProvider = new AutoloadPathProvider(null, null, false);
-        }
+        InstalledVersions::isInstalled('symfony/framework-bundle');
+        InstalledVersions::isInstalled('doctrine/orm');
 
         return Neon::encode([
+            'includes' => [
+                'phpstan-rules.php',
+                'phpstan-paths.php',
+            ],
             'parameters' => [
                 'level' => 8,
-                'paths' => $autoloadPathProvider->getPaths(),
+                'excludePaths' => [
+                    '**/node_modules/*',
+                    '**/vendor/*',
+                ],
+                'type_perfect' => [
+                    'no_mixed_property' => true,
+                    'no_mixed_caller' => true,
+                    'null_over_false' => true,
+                    'narrow_param' => true,
+                    'narrow_return' => true,
+                ],
+                'errorFormat' => 'ticketswap',
+                'editorUrl' => 'phpstorm://open?file=%%file%%&line=%%line%%',
+                'ignoreErrors' => [],
             ],
-        ], true); // Returns $value converted to multiline NEON
+        ], true);
+        // Returns $value converted to multiline NEON
+    }
+
+    private function createRulesProviderConfig(): string
+    {
+        $providersLine = [
+            '    new Algoritma\CodingStandards\Rules\PhpstanRulesProvider(),',
+        ];
+        $providersLine[] = '    new Algoritma\CodingStandards\Rules\ArrayRulesProvider($additionalRules),';
+        $providersLine = implode("\n", $providersLine);
+
+        return <<<EOD
+            \$additionalRules = [];
+            \$rulesProvider = new Algoritma\\CodingStandards\\Rules\\CompositeRulesProvider([
+            {$providersLine}
+            ]);
+            EOD;
+    }
+
+    private function createPathsFile(bool $noDev): string
+    {
+        $autoloadPathProvider = '$autoloadPathProvider = new Algoritma\CodingStandards\AutoloadPathProvider();';
+
+        if ($noDev) {
+            $autoloadPathProvider = '$autoloadPathProvider = new Algoritma\CodingStandards\AutoloadPathProvider(null, null, false);';
+        }
+
+        return <<<EOD
+            <?php
+            
+            {$autoloadPathProvider}
+            return ['parameters' => ['paths' => [\$autoloadPathProvider->getPaths()]]];
+
+            EOD;
+    }
+
+    private function createRulesFile(): string
+    {
+        $rulesProviderConfig = $this->createRulesProviderConfig();
+
+        return <<<EOD
+            <?php
+
+            {$rulesProviderConfig}
+
+            return ['rules' => \$rulesProvider->getRules()];
+
+            EOD;
     }
 }
---
+++ src/Rules/PhpstanRulesProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Algoritma\CodingStandards\Rules;
+
+final class PhpstanRulesProvider extends AbstractRuleProvider
+{
+    public function getRules(): array
+    {
+        return [];
+    }
+}
