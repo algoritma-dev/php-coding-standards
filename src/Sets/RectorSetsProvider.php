a1a97451013d0b345de3b9f04f6d4139ffdb73f5
Raffaele Carelle <raffaele.carelle@algoritma.it>
2024-12-18 14:40:30 +0100

    Add Rector sets and dynamic PHP version handling
    Introduced predefined Rector sets to enhance code quality and standards. Added a method to dynamically determine the PHP-specific set based on the current PHP version, with support for versions 8.2 to 8.4 and error handling for unsupported versions.

--- src/Sets/RectorSetsProvider.php
+++ src/Sets/RectorSetsProvider.php
@@ -4,10 +4,31 @@ declare(strict_types=1);
 
 namespace Algoritma\CodingStandards\Sets;
 
+use Rector\Set\ValueObject\SetList;
+
 class RectorSetsProvider implements SetsProviderInterface
 {
     public function getSets(): array
     {
-        return [];
+        return [
+            SetList::CODE_QUALITY,
+            SetList::EARLY_RETURN,
+            SetList::INSTANCEOF,
+            SetList::DEAD_CODE,
+            $this->getPhpSet(),
+            SetList::TYPE_DECLARATION,
+        ];
+    }
+
+    private function getPhpSet(): string
+    {
+        $version = PHP_VERSION_ID;
+
+        return match (true) {
+            $version >= 80_200 && $version <= 80_299 => SetList::PHP_82,
+            $version >= 80_300 && $version <= 80_399 => SetList::PHP_83,
+            $version >= 80_400 && $version <= 80_499 => SetList::PHP_84,
+            true => throw new \Exception('PHP version not supported'),
+        };
     }
 }
