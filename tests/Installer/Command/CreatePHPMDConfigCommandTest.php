b80b24d2fe423930c63e9c6cbcedefaaee094d67
Raffaele Carelle <raffaele.carelle@algoritma.it>
2025-01-24 16:45:24 +0100

    Add support for generating PHPMD configuration
    Introduced functionality to generate a default PHPMD configuration file, including relevant tests and command integration. This ensures seamless setup of PHPMD rules within the Installer workflow.

---
+++ tests/Installer/Command/CreatePHPMDConfigCommandTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Algoritma\CodingStandardsTest\Installer\Command;
+
+use Algoritma\CodingStandards\Installer\Command\CreatePHPMDConfigCommand;
+use Algoritma\CodingStandards\Installer\Writer\PhpCsConfigWriterInterface;
+use Algoritma\CodingStandardsTest\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreatePHPMDConfigCommandTest extends TestCase
+{
+    public function testGetConfigWriter(): void
+    {
+        $command = new CreatePHPMDConfigCommand();
+        $writer = $command->getConfigWriter();
+        static::assertSame($writer, $command->getConfigWriter());
+    }
+
+    public function testSetConfigWriter(): void
+    {
+        $command = new CreatePHPMDConfigCommand();
+        $writer = $this->prophesize(PhpCsConfigWriterInterface::class);
+        $command->setConfigWriter($writer->reveal());
+        static::assertSame($writer->reveal(), $command->getConfigWriter());
+    }
+
+    /**
+     * @param list<string> $args
+     *
+     * @throws \Exception
+     */
+    #[DataProvider('executeProvider')]
+    public function testExecute(array $args): void
+    {
+        $command = new CreatePHPMDConfigCommand();
+        $writer = $this->prophesize(PhpCsConfigWriterInterface::class);
+        $command->setConfigWriter($writer->reveal());
+
+        $input = new ArgvInput($args, $command->getDefinition());
+        $output = $this->prophesize(OutputInterface::class);
+
+        $writer->writeConfigFile('phpmd.xml', false)
+            ->shouldBeCalled();
+
+        $result = $command->run($input, $output->reveal());
+
+        static::assertSame(0, $result);
+    }
+
+    /**
+     * @return array{string[], bool}[]
+     */
+    public static function executeProvider(): array
+    {
+        return [
+            [
+                ['algoritma-phpmd-create-config'],
+                false,
+            ],
+        ];
+    }
+}
